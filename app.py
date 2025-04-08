b8a5bb39572dfba53c260f47f467c6fa921236ac
Ahmed Bahaj <ahmedomarbahaj@gmail.com>
2025-04-08 14:57:37 +0300

    changed models to scikit

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def predict_injury():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
